898a9c7bdf197b1ba4313a23c4b38a18c7ae4bcc
vantayden <vantayden@gmail.com>
2017-09-23 18:16:43 +0700

    DELETE PROJECT

--- model/function.js
+++
@@ -1,8 +0,0 @@
-function formatDate(date) {
-	const hour = date.getDate();
-	const month = date.getMonth() + 1;
-	const year = date.getFullYear();
-	return `${hour}/0${month}/${year}`;
-}
-
-exports.formatDate = formatDate; 
