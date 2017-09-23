c1ddb9a6042b6ee89173511b84abd149c6b0126d
vantayden <vantayden@gmail.com>
2017-09-23 14:52:18 +0700

    ...

--- model/station/station.js
+++ model/station/station.js
@@ -1,10 +1,9 @@
 require('localenv');
 
-function insertRecord(db, data) {
-  const recordCollection = db.collection(process.env.COLLECTION_STATION);
-  data.time = new Date();
+function insertStation(db, data) {
+  const stationCollection = db.collection(process.env.COLLECTION_STATION);
   return new Promise((resolve, reject) => {
-    recordCollection.insertOne(data)
+    stationCollection.insertOne(data)
         .then((doc) => {
           resolve({error: false, msg: "insert success", insertedId: doc.insertedId})
         }).catch((err) => {
@@ -13,10 +12,10 @@ function insertRecord(db, data) {
   });
 }
 
-function getRecord(db, recordID) {
-  const recordCollection = db.collection(process.env.COLLECTION_RECORD);
+function getStation(db, stationID) {
+  const stationCollection = db.collection(process.env.COLLECTION_RECORD);
   return new Promise((resolve, reject) => {
-    recordCollection.findOne({_id: recordID})
+    stationCollection.findOne({_id: stationID})
         .then((data) => {
           resolve({error: false, msg: "get success", data})
         }).catch((err) => {
@@ -24,3 +23,32 @@ function getRecord(db, recordID) {
     })
   });
 }
+
+function getAllStation(db) {
+  const stationCollection = db.collection(process.env.COLLECTION_RECORD);
+  return new Promise((resolve, reject) => {
+    stationCollection.find({}).toAray()
+        .then((station) => {
+          resolve({error: false, msg: "get success", station})
+        }).catch((err) => {
+      reject({error: true, msg: "Can't GET!"});
+    })
+  });
+}
+
+function updateStation(db, name, pm) {
+  const stationCollection = db.collection(process.env.COLLECTION_STATION);
+  return new Promise((resolve, reject) => {
+    stationCollection.updateOne({ name }, { $set: { lastRecord: pm } })
+        .then(() => {
+          resolve({error: false, msg: "update success"})
+        }).catch((err) => {
+      reject({error: true, msg: "Can't insert!"});
+    })
+  });
+}
+
+exports.insertStation = insertStation;
+exports.getAllStation = getAllStation;
+exports.updateStation = updateStation;
+
